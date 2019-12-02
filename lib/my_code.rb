d2ed5e4ef663baf4c08be062d5bf2516c41b7d10
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-12-02 22:47:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -61,8 +61,7 @@ end
 def reduce_to_any_true(source_array) 
   counter = 0 
   while counter < source_array.length do 
-    if source_array.any?
-    return true 
+
     counter += 1 
   end 
   return false 
